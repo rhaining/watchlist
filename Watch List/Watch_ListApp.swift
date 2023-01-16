98e2945ae0ae8fe7dfe3d85536016dc4b7795b03
Rob Haining <rob@tolar.ws>
2023-01-16 13:46:19 -0500

    Initial Commit

---
+++ Watch List/Watch_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_ListApp.swift
+//  Watch List
+//
+//  Created by Robert Tolar Haining on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
