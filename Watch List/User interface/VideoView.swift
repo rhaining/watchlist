35bf83c4b2959df6395d4b8e134d6e2d070044bc
Rob Haining <rob@tolar.ws>
2023-10-05 22:41:09 -0400

    tweaked videos

--- Watch List/User interface/VideoView.swift
+++ Watch List/User interface/VideoView.swift
@@ -19,40 +19,42 @@ struct VideoView: View {
     
     var body: some View {
         if let displayedVideos = displayedVideos {
-            VStack (alignment: .leading, spacing: 0) {
-                Text("Videos")
-                    .font(.subheader)
-                    .padding(.horizontal)
-                    .padding(.top)
-                ForEach(displayedVideos, id: \.id) { video in
-                    Button {
-                        videoKey = video.key
-                        presentYoutube = true
-                    } label: {
-                        HStack (alignment: .center, spacing: 0) {
-                            Image(systemName: "play.circle")
-                                .frame(width: 60, height: 60)
-                            
-                            Text(video.name ?? video.type ?? "video")
-                                .font(.primary)
-                            Spacer()
+            if displayedVideos.count > 0  {
+                VStack (alignment: .leading, spacing: 0) {
+                    Text("Videos")
+                        .font(.subheader)
+                        .padding(.horizontal)
+                        .padding(.top)
+                    ForEach(displayedVideos, id: \.id) { video in
+                        Button {
+                            videoKey = video.key
+                            presentYoutube = true
+                        } label: {
+                            HStack (alignment: .center, spacing: 0) {
+                                Image(systemName: "play.circle")
+                                    .frame(width: 60, height: 60)
+                                
+                                Text(video.name ?? video.type ?? "video")
+                                    .font(.primary)
+                                    .multilineTextAlignment(.leading)
+                                Spacer()
+                            }
                         }
                     }
-                }
-                if hasMoreVideos {
-                    Button("Display all \(videos!.count) videos…") {
-                        self.hasMoreVideos = false
-                        self.displayedVideos = videos
+                    if hasMoreVideos {
+                        Button("Display all \(videos!.count) videos…") {
+                            self.hasMoreVideos = false
+                            self.displayedVideos = videos
+                        }
+                        .padding()
                     }
-                    .padding()
+                }
+                .background(Color.white.opacity(0.8))
+                .padding(.horizontal)
+                .sheet(isPresented: $presentYoutube) {
+                    YoutubeView(videoKey: $videoKey)
                 }
             }
-            .background(Color.white.opacity(0.8))
-            .padding(.horizontal)
-            .sheet(isPresented: $presentYoutube) {
-                YoutubeView(videoKey: $videoKey)    
-            }
-            
         } else {
             Text("Loading videos…")
                 .onAppear(perform: loadVideos)
